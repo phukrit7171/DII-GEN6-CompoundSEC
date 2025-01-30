31cdb6157ea147c58fcd8ff7d6e0829d1212d138
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-01-30 12:08:08 +0700

    init with maven

---
+++ CompoundSEC/src/main/java/com/pklabtechnology/compoundsec/App.java
@@ -0,0 +1,13 @@
+package com.pklabtechnology.compoundsec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
